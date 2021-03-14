14f2006937974139d37d8a6236bb27255567815b
Anton Kristiono <antonkristiono47@gmail.com>
2021-03-14 08:01:11 +0700

    change layout

---
+++ components/Hero1.js
@@ -0,0 +1,11 @@
+export default function Hero1() {
+  return (
+    <div className="hero bg-gray-100 py-16">
+      <div className="grid grid-cols-6 gap-4">
+        <div className="col-star-2 col-span-4">
+          <img src="/image2.jpg" />
+        </div>
+      </div>
+    </div>
+  )
+}
--- components/HomeLayout.js
+++ components/HomeLayout.js
@@ -1,10 +1,15 @@
 import Nav from './HorizontalMenu'
+import Head from 'next/head'
 
 export default function HomeLayout({ children }) {
     return (
         <>
-            <Nav />
-            {children}
+          <Head>
+            <title>Website Ngaji</title>
+            <link rel="icon" href="/koran.ico" />
+          </Head>
+          <Nav />
+          {children}
         </>
     )
 }
