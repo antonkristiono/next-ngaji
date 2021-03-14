28209dee7afad51103c3834808bb70a843da5394
Anton Kristiono <antonkristiono47@gmail.com>
2021-03-14 08:13:35 +0700

    change cont

--- components/Hero1.js
+++ components/Hero1.js
@@ -1,9 +1,11 @@
 export default function Hero1() {
   return (
     <div className="hero bg-gray-100 py-16">
-      <div className="grid grid-cols-6 gap-4">
-        <div className="col-star-2 col-span-4">
-          <img src="/image2.jpg" />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-6 gap-4">
+          <div className="col-star-2 col-span-4">
+            <img src="/image2.jpg" />
+          </div>
         </div>
       </div>
     </div>
